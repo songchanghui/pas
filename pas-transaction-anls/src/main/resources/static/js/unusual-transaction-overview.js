9e23475cfd4237f98b96562144d5c10d05b0424e
songchanghui <songchanghui@nowledgedata.com.cn>
2019-03-29 14:29:46 +0800

    异常交易代码风格调整

--- pas-transaction-anls/src/main/resources/static/js/unusual-transaction-overview.js
+++ pas-transaction-anls/src/main/resources/static/js/unusual-transaction-overview.js
@@ -1,3 +1,4 @@
+/** @namespace data.totalAmount.rate */
 /**
  * 设置交易信息概况
  * @param data
@@ -5,13 +6,22 @@
 var setOverview = function(data){
     //异常交易-金额(万元)
     var unusualTotalAmount = data.totalAmount.value;
+    var unusualTotalAmountRate = data.totalAmount.rate;
     //异常交易-笔数(笔)
     var unusualTotalNum = data.totalNum.value;
+    var unusualTotalNumRate = data.totalNum.rate;
     //异常交易-服务点数(个)
     var unusualTotalShopNum = data.totalShopNum.value;
+    var unusualTotalShopNumRate = data.totalShopNum.rate;
 
     //设置异常概况
     $('#num1').animateNumber({ number: unusualTotalAmount});
+    $('#num1-rate0').text(unusualTotalAmountRate);
+    $('#num1-rate1').css("width",unusualTotalAmountRate);
     $('#num2').animateNumber({ number: unusualTotalNum });
+    $('#num2-rate0').text(unusualTotalNumRate);
+    $('#num2-rate1').css("width",unusualTotalNumRate);;
     $('#num3').animateNumber({ number: unusualTotalShopNum });
+    $('#num3-rate0').text(unusualTotalShopNumRate);
+    $('#num3-rate1').css("width",unusualTotalShopNumRate);
 }
--- pas-transaction-anls/src/main/resources/static/js/unusual-transaction-region-information.js
+++ pas-transaction-anls/src/main/resources/static/js/unusual-transaction-region-information.js
@@ -4,6 +4,9 @@
 /** @namespace item.totalAmount */
 /** @namespace item.totalNum */
 /** @namespace item.totalShopNum */
+/** @namespace item.totalAmountRate */
+/** @namespace item.totalNumRate */
+/** @namespace item.totalShopNumRate */
 /** @param data   [{"name":"云浮市","totalAmount":38122.52135,"totalNum":668109,"totalShopNum":0},{"name":"揭阳市","totalAmount":8325.66144,"totalNum":76357,"totalShopNum":0}]*/
 var setRegionInformation = function(data){
     data.forEach(function(item,i){
@@ -11,22 +14,25 @@ var setRegionInformation = function(data){
         var totalAmount = item.totalAmount;
         var totalNum = item.totalNum;
         var totalShopNum = item.totalShopNum;
+        var totalAmountRate = item.totalAmountRate;
+        var totalNumRate = item.totalNumRate;
+        var totalShopNumRate = item.totalShopNumRate;
 
         var templateTr = $("#template").clone();
         templateTr.attr("id","region"+i);
         templateTr.css("display","block");
         templateTr.find("#region").text(regionName);
         templateTr.find("#region-amount").text(totalAmount);
-        templateTr.find("#region-amount-percent0").text("40%");
-        templateTr.find("#region-amount-percent1").css("width","40%");
+        templateTr.find("#region-amount-rate0").text(totalAmountRate);
+        templateTr.find("#region-amount-rate1").css("width",totalAmountRate);
 
         templateTr.find("#region-num").text(totalNum);
-        templateTr.find("#region-num-percent0").text("40%");
-        templateTr.find("#region-num-percent1").css("width","40%");
+        templateTr.find("#region-num-rate0").text(totalNumRate);
+        templateTr.find("#region-num-rate1").css("width",totalNumRate);
 
         templateTr.find("#region-service-num").text(totalShopNum);
-        templateTr.find("#region-service-num-percent0").text("40%");
-        templateTr.find("#region-service-num-percent1").css("width","40%");
+        templateTr.find("#region-service-num-rate0").text(totalShopNumRate);
+        templateTr.find("#region-service-num-rate1").css("width",totalShopNumRate);
 
         templateTr.appendTo("#list");
     });
