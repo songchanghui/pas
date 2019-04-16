82562b1daeedcd33035c92a6fa03001b5b18d915
Administrator <123456>
2019-04-16 16:26:15 +0800

    pc自适应

--- pas-transaction-anls/src/main/resources/static/js/rem.js
+++ pas-transaction-anls/src/main/resources/static/js/rem.js
@@ -1,16 +1,14 @@
- (function (doc, win) {
-     var docEl = doc.documentElement,
-         resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-         recalc = function () {
-             var clientWidth = docEl.clientWidth;
-             if (!clientWidth) return;
-             if (clientWidth >= 1920) {
-                 docEl.style.fontSize = '100px'; //1rem  = 100px
-             } else {
-                 docEl.style.fontSize = 100 * (clientWidth / 1920) + 'px';
-             }
-         };
-     if (!doc.addEventListener) return;
-     win.addEventListener(resizeEvt, recalc, false);
-     doc.addEventListener('DOMContentLoaded', recalc, false);
- })(document, window);
+$(function(){
+  var whdef = 100/1920;// 表示1920的设计图,使用100PX的默认值
+  var wH = window.innerHeight;// 当前窗口的高度
+  var wW = window.innerWidth;// 当前窗口的宽度
+  var rem = wW * whdef;// 以默认比例值乘以当前窗口宽度,得到该宽度下的相应FONT-SIZE值
+  $('html').css('font-size', rem + "px");
+});
+$(window).resize(function (){// 绑定到窗口的这个事件中
+  var whdef = 100/1920;// 表示1920的设计图,使用100PX的默认值
+  var wH = window.innerHeight;// 当前窗口的高度
+  var wW = window.innerWidth;// 当前窗口的宽度
+  var rem = wW * whdef;// 以默认比例值乘以当前窗口宽度,得到该宽度下的相应FONT-SIZE值
+  $('html').css('font-size', rem + "px");
+});
